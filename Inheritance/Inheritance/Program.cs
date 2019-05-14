5eb08e1007b062e23dac70a28d1f9322744309ec
Hafidzfidzz <48426232+Hafidzfidzz@users.noreply.github.com>
2019-05-14 14:49:25 +0700

    Add files via upload

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Hafidz", 20);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Thomas", 30, "190302", "Math");
+            person.GetNameAndAge();
+
+            Student student = new Student("Andreas", 19, "10506", "Andreas1234@amikom.ac.id");
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+        }
+    }
+}
